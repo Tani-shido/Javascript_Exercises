57de70ddce7cfc1f98d90b44cfbde3f937cd812d
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-06-23 21:19:36 +0530

    Update Todo_pro.js

--- javascript_Fundamental_Projects/Todo_pro/Todo_pro.js
+++ javascript_Fundamental_Projects/Todo_pro/Todo_pro.js
@@ -1,62 +1,29 @@
-let tasks = [];
 
+    let todos = [];
 
-function renderTasks() {
-    const addButton = document.getElementById("addButton");
-    
-    const completed = document.createElement("button");
-    completed.innerText = "Completed"  
-    completed.addEventListener("click" , function(){
-        listItem.classList.toggle("completed");
-    });
-
-    const unoderedList = document.getElementById("unOdered");
-}
-
-function adTask() {
-    const input = document.getElementById("input");
-    let listData = input.value;
-
-    if(listData.trim() !== "") { 
-        let listItem = document.createElement("li");
-        input.value = "";
+    function addTodo() {
+      todos.push({
+        title: document.querySelector("input").value
+      })
+      render();
     }
-    renderTasks();
 
-function deleteTask() {
-    const deleteItem = document.createElement("button");
-    deleteItem.innerText = "Delete";
-    deleteItem.addEventListener("click" , function(){
-        deleteItem.parentElement.remove()
-    });
-    renderTasks();
-}
-
-
-
-// working of this todo
-// enter input, press add btn, input stored in listData, listData gets verified, new list is created, list named listitem, input gets cleaned
-
-// new complete button is created, button named completed, button got toggle function, new delete button created, button named delete, button got a delete function
-// listdata value stored in listitem, listitem apended to ul, del btn and completed btn appended to listitem.
-
-// I have to make it easier : create 3 functions add, delete, render
-// add will add items to list :-
-// tekes input, store it listdata, verifies it, clears input
-// 
-// delete will delete items in list :-
-// 
-// 
-// render will render all:-
-// 
-// 
-
-        addButton.addEventListener("click" , function(){ 
-
-                listItem.innerText = listData;
-
-                unoderedList.appendChild(listItem);
-                listItem.appendChild(deleteItem);
-                listItem.appendChild(completed);
-            }
-        });
+    function deleteTodo() {
+      
+      render();
+    }
+ 
+    function render() {
+        document.getElementById("todo").innerHTML = "";
+      for (let i = 0; i < todos.length; i++) {
+        const todo = todos[i];
+        const div = document.createElement("div");
+        const h1 = document.createElement("hi");
+        const button = document.createElement("button");
+        button.innerHTML = "Delete"
+        h1.innerHTML = todo.title;
+        div.append(h1);
+        div.append(button);
+        document.getElementById("todo").appendChild(div);
+      }
+    }
