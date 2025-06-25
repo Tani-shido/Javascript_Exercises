a92b2a5e21f82dfd5b35eaa2d2da52188a838515
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-06-25 19:00:34 +0530

    Update Todo_pro.js

--- javascript_Fundamental_Projects/Todo_pro/Todo_pro.js
+++ javascript_Fundamental_Projects/Todo_pro/Todo_pro.js
@@ -8,22 +8,34 @@
       render();
     }
 
-    function deleteTodo() {
-      
+    function deleteTodo(index) {
+      todos.splice(index , 1);
       render();
     }
- 
+
+
     function render() {
         document.getElementById("todo").innerHTML = "";
       for (let i = 0; i < todos.length; i++) {
         const todo = todos[i];
-        const div = document.createElement("div");
+        const divEl = document.createElement("div");
         const h1 = document.createElement("hi");
-        const button = document.createElement("button");
-        button.innerHTML = "Delete"
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        deleteButton.setAttribute("onclick" , "deleteTodo()");
+        
+        const completeButton = document.createElement("input");
+        completeButton.setAttribute('type' , 'checkbox');
+        completeButton.addEventListener("click" , function(){
+          divEl.classList.toggle("completed");
+        });
+
         h1.innerHTML = todo.title;
-        div.append(h1);
-        div.append(button);
-        document.getElementById("todo").appendChild(div);
+        divEl.append(completeButton);
+        divEl.append(h1);
+        divEl.append(deleteButton);
+        document.getElementById("todo").appendChild(divEl);
+        document.querySelector("input").value = "";
       }
     }
