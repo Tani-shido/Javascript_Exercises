8dc8bb0a1f823bcb3291b9668a0938d860467212
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-06-26 18:07:46 +0530

    Update script.js

--- javascript_Fundamental_Projects/Calculator API/script.js
+++ javascript_Fundamental_Projects/Calculator API/script.js
@@ -1,1 +1,39 @@
+const express = require("express");
 
+const app = express();
+
+app.get("/sum/:firstArg/:secondArg", function(req, res) {
+    const a = parseInt(req.params.firstArg);
+    const b = parseInt(req.params.secondArg);
+
+    res.json({
+        ans: a + b
+    })
+});
+
+app.get("/multiply/:firstArg/:secondArg", function(req, res) {
+    const a = parseInt(req.params.firstArg);
+    const b = parseInt(req.params.secondArg);
+    res.json({
+        ans: a * b
+    })
+});
+
+app.get("/divide/:firstArg/:secondArg", function(req, res) {
+    const a = parseInt(req.params.firstArg);
+    const b = parseInt(req.params.secondArg);
+    res.json({
+        ans: a / b
+    })
+
+});
+
+app.get("/subtract/:firstArg/:secondArg", function(req, res) {
+    const a = parseInt(req.params.firstArg);
+    const b = parseInt(req.params.secondArg);
+    res.json({
+        ans: a - b
+    })
+});
+
+app.listen(3000);
