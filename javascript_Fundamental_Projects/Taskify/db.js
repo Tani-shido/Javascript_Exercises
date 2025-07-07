04674ce81621d7d9c77fb61ba4d4a3c06efe5cc9
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-07-07 14:27:17 +0530

    Add files via upload

---
+++ javascript_Fundamental_Projects/Taskify/db.js
@@ -0,0 +1,9 @@
+let tasks = [
+    {id: 1, title: "first", completed: true},
+    {id: 2, title: "second", completed: false},
+    {id: 3, title: "third", completed: false}
+];
+
+let users = []
+
+module.exports = {tasks , users};
